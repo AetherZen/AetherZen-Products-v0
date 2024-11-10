1496ddae1f049665174453274947c2afa5623d56
HalimaSaadia <halimasadia9666@gmail.com>
2024-11-10 18:43:05 +0600

    created initial navbar for large device

---
+++ src/components/shared/navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FiUser } from "react-icons/fi";
+import { FaArrowRight } from "react-icons/fa";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname);
+  const navLinks = [
+    {
+      href: "/",
+      text: "Home",
+    },
+    {
+      href: "/gallary",
+      text: "Website Gallery",
+    },
+    {
+      href: "/documentation",
+      text: "Documentation",
+    },
+    {
+      href: "/contact",
+      text: "Contact Us",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center section-padding-x py-10">
+      <div>
+        <Image src="/logo.png" alt="Logo" height={50} width={186} />
+      </div>
+      <div className="flex gap-x-12">
+        {navLinks?.map((link, idx) => (
+          <Link
+            className={`inline-block font-medium group ${
+              pathname === link?.href ? "text-az-primary" : ""
+            } `}
+            key={idx}
+            href={link?.href}
+          >
+            {link?.text}
+            <span
+              className={`block h-[2px]  ${
+                pathname === link?.href
+                  ? "bg-gradient-to-tr from-az-primary w-full"
+                  : "group-hover:bg-gradient-to-tr group-hover:from-az-primary group-hover:to-[#e42c3800] w-0 group-hover:w-full duration-300"
+              }`}
+            ></span>
+          </Link>
+        ))}
+      </div>
+      <div className="flex gap-3 items-center">
+        <Link href="/">
+          <AiOutlineShoppingCart className="text-2xl"/>
+        </Link>{" "}
+        <Link href="/">
+          <FiUser className="text-2xl" />
+        </Link>
+        <button className="bg-black text-white flex items-center px-4 py-2 gap-2 rounded-full">Contact Us <FaArrowRight className="-rotate-45" /></button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
