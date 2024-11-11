1def2d52339901ca79a7b238ee3815ebed58d7c8
husneara <husnearaa99@gmail.com>
2024-11-11 14:08:01 +0600

    has updated the marquee section

--- src/components/MarqueeSection/MarqueeNews.jsx
+++ src/components/MarqueeSection/MarqueeNews.jsx
@@ -1,19 +1,38 @@
 import Marquee from "react-fast-marquee";
 
+
 const MarqueeNews = () => {
+
+
+ const marqueeData = [
+    {
+      "text": "LIO WEBSITE"
+    },
+    {
+      "text": "BUSINESS WEBSITE"
+    },
+    {
+      "text": "EDUCATION WEBSITE"
+    }
+  ]
+  
+
+
   return (
     <div>
       <Marquee>
         <div className="uppercase flex items-center text-5xl border-red-100 border-t-[1px] border-b-[1px] py-8">
-          <p className=" text-gray-500 font-bold text-center pl-2 pr-2"><span className=" text-black font-extrabold rounded-full px-3">.</span> LIO WEBSITE</p>
-          <p className=" text-black font-bold">
-            <span className=" text-black font-extrabold rounded-full pl-3 pr-3 text-center">.</span>{" "}
-              BUSINESS WEBSITE{" "}
-          </p>
-          <p className="  text-gray-500 font-bold">
-            <span className=" text-gray-500 font-extrabold rounded-full  pr-2 pl-2 text-center">.</span>
-              EDUCATION WEBSITE
-          </p>
+          {marqueeData.map((item, index) => (
+            <p
+              key={index}
+              className={`${
+                index % 2 === 0 ? "text-gray-500" : "text-black"
+              } font-bold flex gap-6 justify-center items-center`}
+            >
+              <span className="rounded-full size-3 bg-black ml-6"></span>
+              <span>{item.text}</span>
+            </p>
+          ))}
         </div>
       </Marquee>
     </div>
