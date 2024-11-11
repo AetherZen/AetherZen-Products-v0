f042584ddea075beffc3387fe5ebaf5cd0f6a17c
husneara <husnearaa99@gmail.com>
2024-11-11 17:00:50 +0600

    has created and complted the whyus section of home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import MarqueeNews from "@/components/MarqueeSection/MarqueeNews";
 import { Skeleton } from "@/components/ui/skeleton";
+import WhyUs from "@/components/WhyUs/WhyUs";
 
 
 export default function Home() {
   return (
     <main>
       <MarqueeNews />
+      <WhyUs />
     </main>
   );
 }
