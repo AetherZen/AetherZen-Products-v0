aa8f21bcf2688cccd05ad6206e742af7ec4a4555
HalimaSaadia <halimasadia9666@gmail.com>
2024-11-11 18:05:26 +0600

    created category section

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "*",
+        },
+      
+      ],
+    },
+    
+  };
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import CategoryHome from "@/components/categoryHome/CategoryHome";
 import { Skeleton } from "@/components/ui/skeleton";
+
 
 export default function Home() {
   return (
     <main className="min-h-[50vh]">
-      
+      <CategoryHome />
     </main>
   );
 }
