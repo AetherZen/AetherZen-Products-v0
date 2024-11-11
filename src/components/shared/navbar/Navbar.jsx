9dfb56dc8ae6624a1c0ec2784a698bb4995c1b32
HalimaSaadia <halimasadia9666@gmail.com>
2024-11-11 11:57:18 +0600

    created initial sideNav

--- src/components/shared/navbar/Navbar.jsx
+++ src/components/shared/navbar/Navbar.jsx
@@ -6,10 +6,12 @@ import { usePathname } from "next/navigation";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { FiUser } from "react-icons/fi";
 import { FaArrowRight } from "react-icons/fa";
+import { SideNav } from "./SideNav";
+import { CiSearch } from "react-icons/ci";
 
 const Navbar = () => {
   const pathname = usePathname();
-  console.log(pathname);
+
   const navLinks = [
     {
       href: "/",
@@ -30,10 +32,19 @@ const Navbar = () => {
   ];
   return (
     <div className="flex justify-between items-center section-padding-x py-10">
-      <div>
-        <Image src="/logo.png" alt="Logo" height={50} width={186} />
+      <div className="flex items-center">
+        <div className="lg:hidden">
+          <SideNav navLinks={navLinks} />
+        </div>
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          height={50}
+          className="h-10 w-32 lg:h-14 lg:w-44"
+          width={186}
+        />
       </div>
-      <div className="flex gap-x-12">
+      <div className=" gap-x-12 hidden lg:flex">
         {navLinks?.map((link, idx) => (
           <Link
             className={`inline-block font-medium group ${
@@ -46,21 +57,26 @@ const Navbar = () => {
             <span
               className={`block h-[2px]  ${
                 pathname === link?.href
-                  ? "bg-gradient-to-tr from-az-primary w-full"
-                  : "group-hover:bg-gradient-to-tr group-hover:from-az-primary group-hover:to-[#e42c3800] w-0 group-hover:w-full duration-300"
+                  ? "bg-gradient-to-r from-az-primary w-full"
+                  : "group-hover:bg-gradient-to-r group-hover:from-az-primary group-hover:to-[#e42c3800] w-0 group-hover:w-full duration-300"
               }`}
             ></span>
           </Link>
         ))}
       </div>
-      <div className="flex gap-3 items-center">
+      <div className="flex gap-5 items-center">
+        <Link className="lg:hidden" href="/">
+          <CiSearch className="text-2xl" />
+        </Link>
         <Link href="/">
-          <AiOutlineShoppingCart className="text-2xl"/>
+          <AiOutlineShoppingCart className="text-2xl" />
         </Link>{" "}
-        <Link href="/">
+        <Link className="hidden lg:block" href="/">
           <FiUser className="text-2xl" />
         </Link>
-        <button className="bg-black text-white flex items-center px-4 py-2 gap-2 rounded-full">Contact Us <FaArrowRight className="-rotate-45" /></button>
+        <button className="bg-black text-white hidden lg:flex items-center px-4 py-2 gap-2 rounded-full">
+          Contact Us <FaArrowRight className="-rotate-45" />
+        </button>
       </div>
     </div>
   );
